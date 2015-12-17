8a1fe3559196dfd9a8edb72efbea82189688f9dc
Vgr E. Barry <vgr255@live.ca>
2015-12-17 13:10:51 -0500

    Add some more channel stuff

--- channel.py
+++ channel.py
@@ -3,17 +3,19 @@ def _check(s):
         raise TypeError("first argument to Channel method "
                         "must be a Channel object")
 
-channels = []
+_channels = []
 
-def get_channel(name):
+exists = _channels.__contains__
+
+def get(name):
     assert isinstance(name, str), "channel name must be a string"
-    for chan in channels:
+    for chan in _channels:
         assert isinstance(chan, Channel), "stored channel must be a Channel"
         if chan.name == name:
             return chan
 
     result = Channel(name)
-    channels.append(result)
+    _channels.append(result)
     return result
 
 class Channel:
@@ -21,6 +23,10 @@ class Channel:
         _check(self)
         self.name = name
         self.users = []
+        self.modes = {
+            "no_parameter": [],
+            "single_parameter": [],
+        }
 
     def u_join(self, user):
         _check(self)
@@ -30,10 +36,11 @@ class Channel:
         _check(self)
         self.users.remove(user)
         if not self.users:
-            channels.remove(self)
+            _channels.remove(self)
 
+    u_quit = u_part
 
-
-
+    def u_mode(self, user, target, modes):
+        _check(self)
 
 

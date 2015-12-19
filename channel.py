de2caf3d8534df7c8e93bcd1145690be13cc8df0
Vgr E. Barry <vgr255@live.ca>
2015-12-19 16:09:30 -0500

    Change handling of Channel instances

--- channel.py
+++ channel.py
@@ -1,26 +1,19 @@
-def _check(s):
-    if not isinstance(s, Channel):
-        raise TypeError("first argument to Channel method "
-                        "must be a Channel object")
+import events
+import parse
 
-_channels = []
+class MissingDict(dict):
+    def __missing__(self, name):
+        result = Channel(name)
+        self[name] = result
+        return result
+
+_channels = MissingDict()
 
 exists = _channels.__contains__
-
-def get(name):
-    assert isinstance(name, str), "channel name must be a string"
-    for chan in _channels:
-        assert isinstance(chan, Channel), "stored channel must be a Channel"
-        if chan.name == name:
-            return chan
-
-    result = Channel(name)
-    _channels.append(result)
-    return result
+get = _channels.__getitem__
 
 class Channel:
     def __init__(self, name):
-        _check(self)
         self.name = name
         self.users = []
         self.modes = {
@@ -28,19 +21,18 @@ class Channel:
             "single_parameter": [],
         }
 
-    def u_join(self, user):
-        _check(self)
+    def u_join(self, data):
         self.users.append(user)
 
-    def u_part(self, user):
-        _check(self)
+    def u_part(self, data):
         self.users.remove(user)
         if not self.users:
-            _channels.remove(self)
+            del _channels[self.name]
 
-    u_quit = u_part
+    def u_quit(self, data):
+        pass
 
     def u_mode(self, user, target, modes):
-        _check(self)
+        pass
 
 

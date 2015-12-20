e908d04f8999f92a3e7058e8f611aa14da3d5558
Vgr E. Barry <vgr255@live.ca>
2015-12-20 07:36:51 -0500

    Spacing nit

--- user.py
+++ user.py
@@ -21,4 +21,4 @@ class User:
         self.channels = []
 
     def __str__(self):
-        return "{self.nick}!{self.ident}@{self.host}".format(self = self)
+        return "{self.nick}!{self.ident}@{self.host}".format(self=self)

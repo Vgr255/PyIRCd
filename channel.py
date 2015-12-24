8bdb002f56cb5f04f70f59b725468c0d70021193
Vgr E. Barry <vgr255@live.ca>
2015-12-23 21:45:20 -0500

    Update channel handling

--- channel.py
+++ channel.py
@@ -15,14 +15,22 @@ get = _channels.__getitem__
 class Channel:
     def __init__(self, name):
         self.name = name
-        self.users = []
-        self.modes = {
-            "no_parameter": [],
-            "single_parameter": [],
-        }
+        self.users = {}
+        self.invite_only = False
+        self.key = None
+        self.throttle = None
+        self.limit = None
+        self.forward = None
+        self.ops = set()
+        self.voices = set()
+        self.bans = set()
+        self.quiets = set()
+        self.exempts = set()
+        self.invites = set()
+        self.modes = set()
 
-    def u_join(self, data):
-        self.users.append(user)
+    def join(self, user, key):
+        self.users[user.name] = user
 
     def u_part(self, data):
         self.users.remove(user)
